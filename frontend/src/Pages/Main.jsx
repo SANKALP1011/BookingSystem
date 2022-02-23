2e67ebdef2160c56736a41da3b170a328b7daa73
SANKALP1011 <tigerishan426@gmail.com>
2022-02-23 18:06:36 +0530

    Database testing phase started....

---
+++ frontend/src/Pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Axios from "axios";
+import { useState } from "react";
+
+export const Main = () =>{
+
+    const [DemoName] = useState();
+
+
+}
+export default Main;
