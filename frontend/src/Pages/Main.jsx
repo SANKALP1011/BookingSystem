8d25ae05dcb72bf0d8c2fb58c88acf98c8b01639
SANKALP1011 <tigerishan426@gmail.com>
2022-02-23 23:35:47 +0530

    Axios implementation started , insert route error....

--- frontend/src/Pages/Main.jsx
+++ frontend/src/Pages/Main.jsx
@@ -4,7 +4,24 @@ import { useState } from "react";
 
 export const Main = () =>{
 
-    const [DemoName] = useState();
+    const [DemoName , setDemoName] = useState('');
+    const TestFunction = () => {
+        Axios.post("http://localhost:3001/",{
+            DemoName: DemoName
+        })
+        .then(() =>{
+            alert("Added");
+        });
+
+    }
+  return <>
+         
+         <input type="text" name="DemoName" onChange={(e) =>{
+             setDemoName(e.target.value);
+         }} />
+         <button type="submit" onClick={TestFunction}>ADD</button>
+    
+    </>
 
 
 }
