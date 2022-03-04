15da061b677dc84b72825a956730920f745a3fe7
SANKALP1011 <tigerishan426@gmail.com>
2022-03-04 18:31:44 +0530

    Woking comit...

--- frontend/src/Pages/BookingPanel/Flight.jsx
+++ frontend/src/Pages/BookingPanel/Flight.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import axios from "axios";
 
 export const Flight = () =>{
+   
+   const [Flights , setFlights] = useState([]);
+   Axios.get("")
+   .then((response) =>{
+       setFlights(response.data);
+   })
+
+   return <>
+   
+   <div className="FlightCard">
+       
+       
+       </div>
+
+   </>
 
 }
 
