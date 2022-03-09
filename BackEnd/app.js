18c66f2b5404d86e13c3c2ba5782618c1d0753e4
SANKALP1011 <tigerishan426@gmail.com>
2022-03-09 23:57:34 +0530

    Backend api error.....

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const e = require("express");
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json);
@@ -22,24 +23,34 @@ coonection.connect(function(err,result){
   }
 })
  
-app.get("/",function(req,res){
-  res.send("Backend side")
+
+// app.post("/api",function(req,res){
+//     const Demo = req.body.DemoName;
+//     console.log(Demo);
+//     const query = "INSERT INTO Demo (DemoName) VALUES '"+Demo+"'";
+//     coonection.query(query  ,function(err,result){
+//       if(err){
+//         console.log(err);
+//       }
+//       else{
+//         console.log("added");
+//       }
+//     })
+// })
+
+app.get("/api/Flights",function(req,res){
+  const getFlights = "Select* from Flights";
+  coonection.query(getFlights,function(err,results){
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(results);
+      res.send(results);
+    }
+  })
 })
 
-app.post("/api",function(req,res){
-    const Demo = req.body.DemoName;
-    console.log(Demo);
-    const query = "INSERT INTO Demo (DemoName) VALUES '"+Demo+"'";
-    coonection.query(query  ,function(err,result){
-      if(err){
-        console.log(err);
-      }
-      else{
-        console.log("added");
-      }
-    })
-})
-
-app.listen("3001",function(){
+app.listen("3008",function(){
   console.log("connected");
 })
