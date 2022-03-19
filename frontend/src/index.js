b38250a7abbb25155910e750725845a79e024b31
SANKALP1011 <tigerishan426@gmail.com>
2022-03-20 00:38:45 +0530

    ....

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Main from "../src/Pages/Main";
+import "../src/Pages/Styles/Card.css";
 
 ReactDOM.render(
   <React.StrictMode>
